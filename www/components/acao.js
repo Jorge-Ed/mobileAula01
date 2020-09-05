8a3890922f2fa88824eb8fa8ca3611d457406ce4
Jorge-Ed <60950040+Jorge-Ed@users.noreply.github.com>
2020-09-05 11:12:05 +0900

    Corrigindo funcionamento.

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,10 +1,10 @@
 // This is a JavaScript file
 
-$(document).on("click", "#somar", function (){
-  var valor1 = $("#valor1").val();
-  var valor2 = $("#valor2").val();
+$(document).on("click", "#Somar", function (){
+  var valor1 = $("#Valor1").val();
+  var valor2 = $("#Valor2").val();
 
   var resultado = parseFloat(valor1) + parseFloat(valor2);
 
-  $("#resultado").val(resultado);
+  $("#Resultado").val(resultado);
 });
